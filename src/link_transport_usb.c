f7d99805ba5b45515ecd09ea34f322f32e755938
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2016-05-23 15:15:59 -0600

    Used default stratify link transport

---
+++ src/link_transport_usb.c
@@ -0,0 +1,52 @@
+/*
+ * link_transport.c
+ *
+ *  Created on: May 23, 2016
+ *      Author: tgil
+ */
+
+#include "link_transport_usb.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <iface/dev/pio.h>
+
+
+static link_transport_phy_t link_transport_open(const char * name, int baudrate);
+
+link_transport_driver_t link_transport_usb = {
+		.handle = -1,
+		.open = link_transport_open,
+		.read = stratify_link_transport_usb_read,
+		.write = stratify_link_transport_usb_write,
+		.close = stratify_link_transport_usb_close,
+		.wait = stratify_link_transport_usb_wait,
+		.flush = stratify_link_transport_usb_flush,
+		.timeout = 500
+};
+
+#define USBDEV_CONNECT_PORT "/dev/pio2"
+#define USBDEV_CONNECT_PINMASK (1<<9)
+
+link_transport_phy_t link_transport_open(const char * name, int baudrate){
+	pio_attr_t attr;
+	link_transport_phy_t fd;
+	int fd_pio;
+
+	//Deassert the Connect line and enable the output
+	fd_pio = open(USBDEV_CONNECT_PORT, O_RDWR);
+	if( fd_pio < 0 ){
+		return -1;
+	}
+	attr.mask = (USBDEV_CONNECT_PINMASK);
+	ioctl(fd_pio, I_PIO_SETMASK, (void*)attr.mask);
+	attr.mode = PIO_MODE_OUTPUT | PIO_MODE_DIRONLY;
+	ioctl(fd_pio, I_PIO_SETATTR, &attr);
+
+	fd = stratify_link_transport_usb_open(name, baudrate);
+
+	ioctl(fd_pio, I_PIO_CLRMASK, (void*)attr.mask);
+	close(fd_pio);
+
+	return fd;
+}
